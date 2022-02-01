31b44d666a1b4b692ac52c5e1de4e3cbf31eef3c
Darya Anikeeva <daanikeeva@gmail.com>
2022-02-01 12:00:54 +0300

    feat: add function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function toReadable (number) {
-  
+        const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+        const firstTen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+        const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+          const strArr = [];
+          const numArr = String(number).split('').reverse();
+          for (i = 0; i < numArr.length; i++) {
+             if (number == 0) {
+              strArr.unshift('zero')
+             }        
+             else if (i == 0) {
+                strArr.unshift(units[numArr[i]])
+             }
+             else if (i == 1 && numArr[i] == 1) {
+              strArr.pop();
+              strArr.unshift(firstTen[numArr[0]])
+             }
+             else if (i == 1 && numArr[i] > 1) {
+              strArr.unshift(tens[numArr[i]])
+             }
+             else if (i == 2) {
+              strArr.unshift('hundred');
+              strArr.unshift(units[numArr[i]])
+             }
+           }
+           const strFilter = strArr.filter(Boolean)
+           return strFilter.join(' ')
 }
